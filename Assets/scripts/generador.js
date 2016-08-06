56d3d8bd444e0d7445c6f34d798ddb498f0184bd
juaniman99 <juaniman.2000@gmail.com>
2016-08-07 00:09:40 +0200

    Guardado de seguridad
    Voy a hacer una prueba con un bucle while para generar el terreno con un
    tamaño de 200 piezas.
    Creo este commit porque no me llevo muy bien con los wiles... xD

---
+++ Assets/scripts/generador.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+var preset1 : GameObject;
+var preset2 : GameObject;
+var preset3 : GameObject;
+var i = 200;
+private var X = 0;
+
+function Start () {
+generar();
+}
+
+function Update () {
+
+}
+
+function generar(){
+	while(i >0){
+	var rand = Random.Range(1, 3);
+	var gene : GameObject;
+	
+		if(rand == 1){
+		gene = preset1;
+		}
+		if(rand == 2){
+		gene = preset2;
+		}
+		if(rand == 3){
+		gene = preset3;
+		}
+	
+	Instantiate(gene, new Vector3(X, 46.5, 0), Quaternion.identity);
+	i = i - 1;
+	}
+	//generar2();
+
+}
+/*
+function generar2(){
+yield WaitForSeconds(7);
+X = X + 40;
+generar();
+}*/
