68d558515875c3b31837a490658cf20c8858ecb9
juaniman99 <juaniman.2000@gmail.com>
2016-08-06 20:57:35 +0200

    Control del personahe'

---
+++ Assets/script/controles.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+import UnityEngine.UI;
+import System.Collections.Generic;
+
+var Control:GameObject;
+var jugador:GameObject;
+var texto: UI.Text;
+
+private var velocidad = 0.2;
+
+function Start () {
+
+}
+
+function Update () {
+print(Input.touchCount);
+	var hit:RaycastHit;
+	var ray:Ray =Camera.main.ScreenPointToRay (Input.mousePosition);
+
+	if(Input.touchCount == 1){
+	if(Physics.Raycast(ray, hit, 20)){
+		if(hit.collider.transform.name == "control_derecho"){
+		jugador.transform.Translate(Vector3(0, 0, -velocidad));		
+		}
+		
+		if(hit.collider.transform.name == "control_izquierdo"){
+		jugador.transform.Translate(Vector3(0, 0, velocidad));
+		}
+	
+	}	
+	}
+
+}
