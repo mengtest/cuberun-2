714d0ae73a451651cefad455e90c330907e524b6
juaniman99 <juaniman.2000@gmail.com>
2016-08-07 13:10:30 +0200

    Raycast fixed
    Detector de colisiones laterales y adición del detector frontal.

--- Assets/scripts/Cube.js
+++ Assets/scripts/Cube.js
@@ -8,17 +8,18 @@ function Start () {
 }
 
 function Update () {
+	this.transform.Translate(Vector2(velocidad, 0));
+	velocidad = velocidad * 1.0001;
+	
 	var hit:RaycastHit;
 	var izquierda = Vector3(0, 0, 0.51);
 	var derecha = Vector3(0, 0, -0.51);
 	
 	//var ray:Ray = Vector3(this.position, this.position, this.position);
+	//Para hacer experimentos y ajustar los raycast.
 	Debug.DrawRay(transform.position, izquierda, Color.red, 2);
 	Debug.DrawRay(transform.position, derecha, Color.green, 2);
-		
-	this.transform.Translate(Vector2(velocidad, 0));
-	velocidad = velocidad * 1.0001;
-
+	Debug.DrawRay(Vector3(transform.position.x + 0.5, 0, transform.position.z -0.5), Vector3(0, 0, 1), Color.blue, 0.1);
 
 
 	if(Physics.Raycast(transform.position, derecha, hit, 0.51)){
@@ -29,8 +30,19 @@ function Update () {
 	}
 	if(Physics.Raycast(transform.position, izquierda, hit, 0.51)){
 		if(hit.collider.transform.tag == "pared"){
-		controles.enableLeft = false;
+			controles.enableLeft = false;
 	}}else{
 		controles.enableLeft = true;
 	}
+	
+	if(Physics.Raycast(Vector3(transform.position.x + 0.5, 0, transform.position.z -0.5), Vector3(0, 0, 1), hit, 0.51)){
+		if(hit.collider.transform.tag == "pared"){
+			fin();
+		}
+	}
+}
+
+function fin(){
+print("FIN DEL JUEGO");
+Application.Quit();
 }
