a225034cdea2934d1ffefa231f315d732d96d0ef
juaniman99 <juaniman.2000@gmail.com>
2016-08-08 00:01:00 +0200

    Sonidos corregidos
    Cada vez que se muestra un cubo en la pantalla, reproducirá un sonido de
    "plot".
    Depende de la posición en Z del bloque, sonará el sonido más agudo o
    menos creando así una especie de """"sintonia""""" o """ritmo"""

--- Assets/scripts/animador.js
+++ Assets/scripts/animador.js
@@ -6,7 +6,7 @@ function OnTriggerEnter(col : Collider){
 	if(col.tag == "pared"){
 		rand = Random.Range(1, 11);
 		col.GetComponent(Animation).Play();
-	
+		sonido(col);
 		if(rand < 10){
 			miniup(col);
 		}
@@ -19,8 +19,8 @@ function miniup(col : Collider){
 	col.GetComponent(Animation).Play("miniup");
 }
 
-function sonido(){
+function sonido(col : Collider){
 	
-	//this.gameObject.GetComponent(AudioSource).pitch -= Time.deltaTime * 5 / 5;	
+	this.gameObject.GetComponent(AudioSource).pitch = col.GetComponent(Transform).position.z * 1.2;	
 	this.gameObject.GetComponent(AudioSource).Play();
 }
