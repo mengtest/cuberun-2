e8a6949f24aed65e3acb51d8179573c1d65947af
juaniman99 <juaniman.2000@gmail.com>
2016-08-07 12:32:46 +0200

    Colisiones mediante RAYCAST

--- Assets/scripts/Cube.js
+++ Assets/scripts/Cube.js
@@ -1,12 +1,36 @@
 ﻿#pragma strict
 
-static var velocidad = 0.27;
+//static var velocidad = 0.27;
+static var velocidad = 0.08;
 
 function Start () {
 
 }
 
 function Update () {
-this.transform.Translate(Vector2(velocidad, 0));
-velocidad = velocidad * 1.00001;
+	var hit:RaycastHit;
+	var izquierda = Vector3(0, 0, 0.51);
+	var derecha = Vector3(0, 0, -0.51);
+	
+	//var ray:Ray = Vector3(this.position, this.position, this.position);
+	Debug.DrawRay(transform.position, izquierda, Color.red, 2);
+	Debug.DrawRay(transform.position, derecha, Color.green, 2);
+		
+	this.transform.Translate(Vector2(velocidad, 0));
+	velocidad = velocidad * 1.0001;
+
+
+
+	if(Physics.Raycast(transform.position, derecha, hit, 0.51)){
+		if(hit.collider.transform.tag == "pared"){
+			controles.enableRight = false;
+	}}else{
+		controles.enableRight = true;
+	}
+	if(Physics.Raycast(transform.position, izquierda, hit, 0.51)){
+		if(hit.collider.transform.tag == "pared"){
+		controles.enableLeft = false;
+	}}else{
+		controles.enableLeft = true;
+	}
 }
--- Assets/scripts/controles.js
+++ Assets/scripts/controles.js
@@ -3,7 +3,8 @@ import UnityEngine.UI;
 import System.Collections.Generic;
 
 var jugador:GameObject;
-
+static var enableRight = true;
+static var enableLeft = true;
 private var velocidad = 0.14;
 
 function Start () {
@@ -17,21 +18,25 @@ function Update () {
 	if(Input.touchCount == 1){
 	if(Physics.Raycast(ray, hit, 20)){
 		if(hit.collider.transform.name == "control_derecho"){
-		jugador.transform.Translate(Vector3(0, 0, -velocidad));		
-		}
+		if(enableRight == true){
+		jugador.transform.Translate(Vector3(0, 0, -velocidad));
+		}}
 		
 		if(hit.collider.transform.name == "control_izquierdo"){
+		if(enableLeft == true){
 		jugador.transform.Translate(Vector3(0, 0, velocidad));
-		}
+		}}
 	
 	}	
 	}
 	//Controladores alternativos para el testeo en el ordenador.
 	if(Input.GetKey(KeyCode.RightArrow)){
+	if(enableRight == true){
 	jugador.transform.Translate(Vector3(0, 0, -velocidad));	
-	}
+	}}
 	if(Input.GetKey(KeyCode.LeftArrow)){
+	if(enableLeft == true){
 	jugador.transform.Translate(Vector3(0, 0, velocidad));	
-	}
+	}}
 
 }
