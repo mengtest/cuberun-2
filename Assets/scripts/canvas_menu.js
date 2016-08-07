cce12474079031b74572bf6f9674cc125faef3fe
juaniman99 <juaniman.2000@gmail.com>
2016-08-08 01:46:17 +0200

    Buenas noches
    Me he quedado asignando las animaciones en el código del canvas.

--- Assets/scripts/canvas_menu.js
+++ Assets/scripts/canvas_menu.js
@@ -1,4 +1,4 @@
-﻿#pragma strict
+﻿import UnityEngine.UI;
 
 var objetosPausa : GameObject;
 var pausatxt : GameObject;
@@ -10,12 +10,23 @@ function iniciar (){
 	Application.LoadLevel("nvl1");
 }
 
-function pausa(){
-	print("HEEEELOOOO XD");
-	//if(objetosPausa.active == false){
+function pausa (){
+
 		objetosPausa.SetActive(true);
 		pausatxt.GetComponent(Animation).Play();
 		reanudarbtn.GetComponent(Animation).Play();
 		ajustesbtn.GetComponent(Animation).Play();
-	//}
+}
+
+function reanudar (){
+	
+		pausatxt.GetComponent(Animation).Play("pausatxtout");
+		reanudarbtn.GetComponent(Animation).Play();
+		ajustesbtn.GetComponent(Animation).Play();
+		reanudar2();
+}
+
+function reanudar2(){
+			yield WaitForSeconds(1.7);
+			objetosPausa.SetActive(false);
 }
