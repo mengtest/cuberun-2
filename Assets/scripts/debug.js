1ca62473b720684e8d620a2f57c64bd1516f9506
juaniman99 <juaniman.2000@gmail.com>
2016-08-11 17:41:14 +0200

    Muchas mas mejoras y correcciones
    Ademas en el modo debug se puede generar texto si en la consola se
    escribe "dropvoid"

--- Assets/scripts/debug.js
+++ Assets/scripts/debug.js
@@ -3,6 +3,7 @@
 var velocidad = 0.2;
 var slider : GameObject;
 var slider1 : GameObject;
+var comandText : GameObject;
 
 function Start () {
 	Cube.velocidad = velocidad;
@@ -11,4 +12,14 @@ function Start () {
 function Update () {
 	Cube.velocidad = slider.GetComponent(Slider).value;
 	this.GetComponent(Camera).orthographicSize = slider1.GetComponent(Slider).value;
+}
+
+function input(){
+	if(comandText.GetComponent(InputField).text == "dropvoid"){
+	this.GetComponent(generador).enabled = false;
+	this.GetComponent(dropvoid_generator).enabled = true;
+	}else{
+	this.GetComponent(generador).enabled = true;
+	this.GetComponent(dropvoid_generator).enabled = false;
+	}
 }
---
+++ Assets/scripts/dropvoid_generator.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+var variatorZ = 0.0;
+var dropvoid : GameObject;
+
+
+private var i = 70;
+private var X = 0;
+
+function Start () {
+
+	generar();
+}
+
+function Update () {
+	if(GameObject.Find("Main Camera").transform.position.x + 70 > X){
+		generar();
+	}
+}
+
+function generar(){
+	
+	var rand = Random.Range(1, 9);
+	var gene : GameObject;
+	
+	gene = dropvoid;
+		
+	Instantiate(gene, new Vector3(X, 46.5, variatorZ), Quaternion.identity);
+	X = X + 40;
+	
+
+}
